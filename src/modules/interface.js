fdb2edba7bb95fa5281ba860302ea40822ba8c46
HarryAhnHS <hongsuhahn@gmail.com>
2024-07-19 14:07:49 +0800

    feat: mobile header touchup

--- src/modules/interface.js
+++ src/modules/interface.js
@@ -174,7 +174,7 @@ const Interface = (() => {
             // Need to subtract arbitrary value - 
             sections.forEach((section) => {
               const sectionTop = section.offsetTop;
-              if (window.scrollY >= sectionTop - 100) { 
+              if (window.scrollY >= sectionTop - 60) { 
                 current = section.getAttribute("id"); }
             });
           
